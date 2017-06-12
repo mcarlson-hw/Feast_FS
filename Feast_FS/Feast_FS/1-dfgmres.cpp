7e359a3829155ed3207bdaa7d6e3f8ac3f396020
mcarlson-hw <max@carlsoncpas.com>
2017-06-12 15:45:44 -0600

    More changes from Kingspeak testing

--- Feast_FS/Feast_FS/1-dfgmres.cpp
+++ Feast_FS/Feast_FS/1-dfgmres.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	// === Main Execution Section ===
-	const int d = 3;
+	const int d = 50;
 	const MKL_INT N = d*d*d;
 	double* X;
 	double* B;
@@ -27,6 +27,9 @@ int main(int argc, char **argv)
 	double* dpar;
 	double* tmp;
 	MKL_INT itercount = 0;
+	int itercount2 = 0;
+	double* tmp1;
+	double* tmp2;
 	CubeFD cl(d, d, d, rank, size, MPI_COMM_WORLD);
 
 	if (rank == 0)
@@ -43,44 +46,97 @@ int main(int argc, char **argv)
 			B[i] = (double)(i + 1);
 		}
 
+		// Initialize GMRES (do once)
 		dfgmres_init(&N, X, B, &RCI_request, ipar, dpar, tmp);
 		cout << "Init: RCI_request = " << RCI_request << endl;
+		ipar[4] = 600;
 
+		// Check parameters for consistency (not required unless parameters manually changed)
 		dfgmres_check(&N, X, B, &RCI_request, ipar, dpar, tmp);
-		cout << "Init: RCI_request = " << RCI_request << endl;
+		cout << "Check: RCI_request = " << RCI_request << endl;
+	}
 
-		dfgmres(&N, X, B, &RCI_request, ipar, dpar, tmp);
-		cout << "Init: RCI_request = " << RCI_request << endl;
-
-		cout << "tmp output = [";
-		for (int i = 0; i < N; i++)
-			cout << tmp[ipar[21] - 1 + i] << " ";
-		cout << endl;
-
-		cl.ApplyA(&tmp[ipar[21] - 1], &tmp[ipar[22] - 1], MPI_COMM_WORLD);
-
-
-
-		dfgmres(&N, X, B, &RCI_request, ipar, dpar, tmp);
-		cout << "Init: RCI_request = " << RCI_request << endl;
-
-		cout << "tmp output = [";
-		for (int i = 0; i < N; i++)
-			cout << tmp[ipar[21] - 1 + i] << " ";
-		cout << endl;
-
-		dfgmres(&N, X, B, &RCI_request, ipar, dpar, tmp);
-		cout << "Init: RCI_request = " << RCI_request << endl;
-
-		cout << "Itercount = " << itercount << endl;
-		dfgmres_get(&N, X, B, &RCI_request, ipar, dpar, tmp, &itercount);
-		cout << "Init: RCI_request = " << RCI_request << endl;
-		cout << "Itercount = " << itercount << endl;
-
-		cout << "X = [";
-		for (int i = 0; i < N; i++)
-			cout << X[i] << " ";
-		cout << endl;
+	while (itercount2 < 2000)
+	{
+		if (rank == 0) dfgmres(&N, X, B, &RCI_request, ipar, dpar, tmp);
+		MPI_Bcast(&RCI_request, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		if (RCI_request == 1)
+		{
+			if (rank == 0)
+			{
+				tmp1 = &tmp[ipar[21] - 1];
+				tmp2 = &tmp[ipar[22] - 1];
+			}
+			cl.ApplyA(tmp1, tmp2, MPI_COMM_WORLD);
+		}
+		if (RCI_request == 2)
+		{
+			// Do nothing for now
+		}
+		if (RCI_request == 3)
+		{
+			// Do nothing for now
+		}
+		if (RCI_request == 4)
+		{
+			if (rank == 0)
+			{
+				if (dpar[6] < 1e-11)
+				{
+					cout << "gmres has converged.\n";
+					cout << "dpar[6] = " << dpar[6] << endl;
+					dfgmres_get(&N, X, B, &RCI_request, ipar, dpar, tmp, &itercount);
+					itercount2 = 1000000;
+					break;
+				}
+				else
+				{
+					cout << "dpar[6] = " << dpar[6] << endl;
+				}
+			}
+		}
+		if (RCI_request == 0)
+		{
+			if (rank == 0)
+			{
+				cout << "gmres has completed but did not converge to expected tolerance.\n";
+				dfgmres_get(&N, X, B, &RCI_request, ipar, dpar, tmp, &itercount);
+			}
+			itercount2 = 1000000;
+			break;
+		}
+		if (RCI_request < 0)
+		{
+			if (rank == 0)
+			{
+				cout << "Something has gone wrong!\n";
+				cout << "RCI_request = " << RCI_request << endl;
+			}
+		}
+		itercount2++;
+	}
+	if (rank == 0)
+	{
+		if (RCI_request == 0)
+		{
+			if (N < 100)
+			{
+				cout << "X = [ ";
+				for (int i = 0; i < N; i++)
+					cout << X[i] << " ";
+				cout << "]\n";
+			}
+			else
+			{
+				cout << "x = [ ";
+				for (int i = 0; i < 10; i++)
+					cout << X[i] << " ";
+				cout << "\n...\n";
+				for (int i = 0; i < 10; i++)
+					cout << X[N - 10 + i] << " ";
+				cout << "]\n";
+			}
+		}
 	}
 	// ==============================
 
